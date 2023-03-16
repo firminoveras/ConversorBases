aae12d09f9f32f3dd667b14a8cab51a18376d133
firmino <firmino.veras@gmail.com>
2023-03-16 14:02:31 -0300

    -- Animações e correções em bases mais altas

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ let inBase = 2
 let outBase = 10
 let lastBase = LAST_BASE_IN;
 
+function validBase(base){
+	return base >= 2 && base <= 36 && /^[0-9]{1,2}$/.test(base)
+}
+
 function convertToDec(num, base) {
 	num = num.toString()
 	num = num.toUpperCase();
-	if (base < 2 || base > 35) throw new Error("Base não suportada!");
+	if (!validBase(base)) throw new Error("Base não suportada!");
 	let exp;
 	let result = 0.0;
 
@@ -18,7 +22,7 @@ function convertToDec(num, base) {
 		exp = -1;
 		for (var i = 0; i < fracNumber.length; i++) {
 			let number;
-			if (fracNumber.charCodeAt(i) >= 48 && fracNumber.charCodeAt(i) < 48 + base)
+			if (fracNumber.charCodeAt(i) >= 48 && fracNumber.charCodeAt(i) < 48 + Math.min(base,9))
 				number = Number(fracNumber[i]);
 			else if (fracNumber.charCodeAt(i) >= 65 && fracNumber.charCodeAt(i) < (55 + base) && base > 10)
 				number = (fracNumber.charCodeAt(i)) - 55;
@@ -30,20 +34,23 @@ function convertToDec(num, base) {
 	exp = 0;
 	for (var i = num.length - 1; i >= 0; i--) {
 		let number;
-		if (num.charCodeAt(i) >= 48 && num.charCodeAt(i) < 48 + base)
+		if (num.charCodeAt(i) >= 48 && num.charCodeAt(i) < 48 + Math.min(base,9))
 			number = Number(num[i]);
-		else if (num.charCodeAt(i) >= 65 && num.charCodeAt(i) < (55 + base) && base > 10)
+		else if (num.charCodeAt(i) >= 65 && num.charCodeAt(i) < (55 + base) && base > 10){
+
 			number = (num.charCodeAt(i)) - 55;
+		}
 		else throw new Error("Algarismo inválido!");
 		result += number * (base ** exp);
+		//console.log(number+"x"+base+"^"+exp);
 		exp++;
 	}
 	return result;
 }
 
 function convertFromDec(num, base) {
-	if(base == 10) return num
-	if (base < 2 || base > 35) throw new Error("Base não suportada!");
+	if (base == 10) return num
+	if (!validBase(base)) throw new Error("Base não suportada!");
 	let n = Number(num)
 	let results = [];
 	while (n > 0) {
@@ -63,12 +70,12 @@ function convertFromDec(num, base) {
 	return out;
 }
 
-
 function convert(input, inputBase, outputBase) {
 	return convertFromDec(convertToDec(input, inputBase), outputBase)
 }
 
 function updateConversion() {
+	setBaseSelectorVisible(false)
 	let text = document.getElementById('numberInput').value
 	document.getElementById('numberOutput').style.fontSize = '40px'
 	document.getElementById('numberOutput').value = ''
@@ -97,90 +104,52 @@ function baseSelectorCustomApply() {
 	}
 }
 
+function updateBaseOfLastElement(base) {
+	if (lastBase == LAST_BASE_IN) {
+		document.getElementById('inChangeButton').innerHTML = base.toString()
+		inBase = base
+	} else {
+		document.getElementById('outChangeButton').innerHTML = base.toString()
+		outBase = base
+	}
+	updateConversion();
+}
 
+function setBaseSelectorVisible(visibility) {
+	if (visibility)
+		document.getElementById('inDiv').classList.add('open')
+	else
+		document.getElementById('inDiv').classList.remove('open')
+
+}
 
 (function (window, document, undefined) {
 	window.onload = init;
 	function init() {
-
-		document.getElementById('numberInput').addEventListener('click', function (evt) {
-			lastBase = LAST_BASE_IN;
-			document.getElementById('arrowUp').style.visibility = 'visible'
-			document.getElementById('backgroundSelectorDiv').style.visibility = 'visible'
-			document.getElementById('arrowDown').style.visibility = 'hidden'
-		})
-		document.getElementById('numberOutput').addEventListener('click', function (evt) {
-			lastBase = LAST_BASE_OUT;
-			document.getElementById('arrowUp').style.visibility = 'hidden'
-			document.getElementById('arrowDown').style.visibility = 'visible'
-			document.getElementById('backgroundSelectorDiv').style.visibility = 'visible'
-		})
-
-		document.getElementById('numberInput').addEventListener('input', function (evt) {
-			updateConversion();
-		})
-
 		document.getElementById('inChangeButton').addEventListener('click', () => {
+			document.getElementById('inDiv').classList.toggle('open')
 			lastBase = LAST_BASE_IN;
 			document.getElementById('arrowUp').style.visibility = 'visible'
-			document.getElementById('backgroundSelectorDiv').style.visibility = 'visible'
 			document.getElementById('arrowDown').style.visibility = 'hidden'
+			document.getElementById('backgroundSelectorDiv').style.visibility = 'visible'
 		})
 
 		document.getElementById('outChangeButton').addEventListener('click', () => {
+			document.getElementById('inDiv').classList.toggle('open')
 			lastBase = LAST_BASE_OUT;
 			document.getElementById('arrowUp').style.visibility = 'hidden'
 			document.getElementById('arrowDown').style.visibility = 'visible'
 			document.getElementById('backgroundSelectorDiv').style.visibility = 'visible'
 		})
 
-		document.getElementById('baseSelector2').addEventListener('click', () => {
-			if (lastBase == LAST_BASE_IN) {
-				document.getElementById('inChangeButton').innerHTML = '2'
-				inBase = 2
-			} else {
-				document.getElementById('outChangeButton').innerHTML = '2'
-				outBase = 2
-			}
-			updateConversion();
-		})
-		document.getElementById('baseSelector8').addEventListener('click', () => {
-			if (lastBase == LAST_BASE_IN) {
-				document.getElementById('inChangeButton').innerHTML = '8'
-				inBase = 8
-			} else {
-				document.getElementById('outChangeButton').innerHTML = '8'
-				outBase = 8
-			}
-			updateConversion();
-		})
-		document.getElementById('baseSelector10').addEventListener('click', () => {
-			if (lastBase == LAST_BASE_IN) {
-				document.getElementById('inChangeButton').innerHTML = '10'
-				inBase = 10
-			} else {
-				document.getElementById('outChangeButton').innerHTML = '10'
-				outBase = 10
-			}
-			updateConversion();
-		})
-		document.getElementById('baseSelector16').addEventListener('click', () => {
-			if (lastBase == LAST_BASE_IN) {
-				document.getElementById('inChangeButton').innerHTML = '16'
-				inBase = 16
-			} else {
-				document.getElementById('outChangeButton').innerHTML = '16'
-				outBase = 16
-			}
-			updateConversion();
-		})
+		document.getElementById('numberInput').addEventListener('input', function (evt) { updateConversion(); })
 
-		document.getElementById('baseSelectorCustom').addEventListener('input', function (evt) {
-			baseSelectorCustomApply()
-		})
-		document.getElementById('baseSelectorCustom').addEventListener('click', function (evt) {
-			baseSelectorCustomApply()
-		})
+		document.getElementById('baseSelector2').addEventListener('click', () => updateBaseOfLastElement(2))
+		document.getElementById('baseSelector8').addEventListener('click', () => updateBaseOfLastElement(8))
+		document.getElementById('baseSelector10').addEventListener('click', () => updateBaseOfLastElement(10))
+		document.getElementById('baseSelector16').addEventListener('click', () => updateBaseOfLastElement(16))
+
+		document.getElementById('baseSelectorCustom').addEventListener('keypress', function (evt) { if (evt.key == 'Enter') baseSelectorCustomApply() })
 	}
 })(window, document, undefined);
 
